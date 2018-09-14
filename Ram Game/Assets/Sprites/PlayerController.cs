d53906cf3bd662732ac59143fb9184d8691e819c
Cuttlefish <zh4n@live.co.uk>
2018-09-14 18:27:14 +0100

    Added basic platformer functionality

---
+++ Ram Game/Assets/Sprites/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed; // default 8
+	public float jumpHeight; // default 12
+	public Transform feetPos;
+	public LayerMask groundLayer;
+
+	private Rigidbody2D rb;
+	private float moveInput;
+	private bool isGrounded;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		moveInput = Input.GetAxisRaw ("Horizontal");
+		rb.velocity = new Vector2 (moveInput * speed, rb.velocity.y);
+	}
+
+	void Update () {
+
+		isGrounded = Physics2D.OverlapBox (feetPos.position, feetPos.GetComponent<BoxCollider2D> ().size, 0, groundLayer);
+
+		if (isGrounded && Input.GetKeyDown (KeyCode.Space)) {
+			rb.velocity = Vector2.up * jumpHeight;
+		}
+
+	}
+}
