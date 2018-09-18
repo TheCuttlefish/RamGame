fc098b9846be3f4bbbceef8b0a4ae6db8e03440e
Cuttlefish <zh4n@live.co.uk>
2018-09-18 04:08:58 +0100

    Added Dash mechanic

--- Ram Game/Assets/Sprites/PlayerController.cs
+++ Ram Game/Assets/Sprites/PlayerController.cs
@@ -5,44 +5,69 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 8;
+	public float dash = 10;
 	public float jumpHeight = 12;
 	public Transform feetPos;
 	public LayerMask groundLayer;
 	public Animator animator;
 
+	private float internal_dash;
 	private Rigidbody2D rb;
 	private float moveInput;
 	private bool isGrounded;
-
+	private bool allowDash = true;
+	private int dashDuration = 15;
+	private int dashCounter;
 	void Start () {
+
 		rb = GetComponent<Rigidbody2D> ();
 	}
 
 	void FixedUpdate () {
 
 		moveInput = Input.GetAxisRaw ("Horizontal");
-		rb.velocity = new Vector2 (moveInput * speed, rb.velocity.y);
+		rb.velocity = new Vector2 ((moveInput * speed) + internal_dash, rb.velocity.y);
 		animator.SetFloat ("Speed", Mathf.Abs (moveInput));
-		if (rb.velocity.x > 0) {
 
+		if (rb.velocity.x > 0) {
 			transform.localScale = new Vector3 (1, transform.localScale.y, transform.localScale.z);
 		}
 		if (rb.velocity.x < 0) {
 			transform.localScale = new Vector3 (-1, transform.localScale.y, transform.localScale.z);
+		}
+
+	}
+	void Jump () {
+
+		if (isGrounded && Input.GetButtonDown ("Jump")) {
+			rb.velocity = Vector2.up * jumpHeight;
+		}
+
+		isGrounded = Physics2D.OverlapBox (feetPos.position, feetPos.GetComponent<BoxCollider2D> ().size, 0, groundLayer);
+		animator.SetBool ("IsJumping", !isGrounded);
+	}
+	void DashAttack () {
+		if (Input.GetButtonDown ("Dash") && allowDash) {
+			allowDash = false;
+			dashCounter = 0;
+
+			internal_dash = dash * transform.localScale.x;
 
 		}
+
+		if (dashCounter < dashDuration) {
+			dashCounter++;
+		} else {
+			internal_dash = 0;
+			allowDash = true;
+		}
+
+		animator.SetBool ("isDashing", !allowDash);
 	}
 
 	void Update () {
-
-		isGrounded = Physics2D.OverlapBox (feetPos.position, feetPos.GetComponent<BoxCollider2D> ().size, 0, groundLayer);
-
-		if (isGrounded && (Input.GetAxis ("Vertical") > 0 || Input.GetButtonDown("Fire1"))) {
-			rb.velocity = Vector2.up * jumpHeight;
-
-		}
-		animator.SetBool ("IsJumping", !isGrounded);
-	
-
+		Jump ();
+		DashAttack ();
 	}
+
 }
